ffda279fae5b63a6edd5a3a2858d059d88767274
poorojepse <poorojepse@gmail.com>
2017-02-28 01:49:13 +0800

    Add files via upload

---
+++ assets/js/functions.js
@@ -0,0 +1,40 @@
+$(document).ready( function() {
+   autoAnimate();
+})
+
+$(document).scroll( function() {
+   animate();
+   parallax();
+})
+
+function animate() {
+   
+   var trigAnimate = $(window).scrollTop() + ( $(window).height() * .5 );
+   
+   $('.viewport').addClass('show-viewport');
+   
+   if ( trigAnimate > $('#hero').offset().top ) {
+      $('#hero .hero-container').addClass('show-hero');
+   }
+}
+
+function autoAnimate() {   
+   var animateService = setInterval(function() {
+      var randNum = Math.floor(Math.random() * $('.service > a').length) + 1;   
+      $('.service > a').eq(randNum).addClass('play-service').siblings().removeClass('play-service');
+   }, 800);
+   
+   $('.service > a').hover(function() {
+      clearInterval(animateService);
+      $('.service > a').removeClass('play-service');
+   }, function() {
+      autoAnimate();
+   })
+}
+
+function parallax() {
+   var scrollTop = $(this).scrollTop();
+   
+   $('#hero').css('background-position', 'center ' + -(scrollTop * .4) + 'px');
+}
+   
